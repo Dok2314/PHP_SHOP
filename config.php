9d3410ad0baa03e7e19597d3ceaee2abccbf432b
Dok2314 <koversundanil@gmail.com>
2022-09-03 16:06:53 +0300

    Added configuration to site

---
+++ config.php
@@ -0,0 +1,15 @@
+<?php
+
+defined('VG_ACCESS') or die('ACCESS DENIED!');
+
+ini_set('error_reporting', E_ALL);
+ini_set('display_startup_errors', 1);
+ini_set('display_errors', 1);
+
+const SITE_URL = 'http://phpshop:8889/';
+const PATH     = '/';
+
+const HOST      = 'localhost';
+const USER      = 'root1';
+const PASSWORD  = 'root';
+const DB_NAME   = 'php_mvc_shop';
--- index.php
+++ index.php
@@ -1,4 +1,9 @@
 <?php
-ini_set('error_reporting', E_ALL);
-ini_set('display_startup_errors', 1);
-ini_set('display_errors', 1);
+
+const VG_ACCESS = true;
+
+header('Content-Type:text/html;charset-utf-8');
+session_start();
+
+require_once "config.php";
+require_once "core/base/settings/internal_settings.php";
