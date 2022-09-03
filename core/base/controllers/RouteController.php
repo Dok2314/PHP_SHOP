cb086d0dbcadaf1e6537cb612571309e18dd89a3
Dok2314 <koversundanil@gmail.com>
2022-09-03 16:58:55 +0300

    Added RouterController and RouteException classes

---
+++ core/base/controllers/RouteController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace core\base\controllers;
+
+class RouteController
+{
+    private static $instance;
+
+    private function __construct()
+    {
+    }
+
+    private function __clone()
+    {
+    }
+
+    public static function getInstance(): RouteController
+    {
+        if(self::$instance instanceof self) {
+            return self::$instance;
+        }
+
+        return self::$instance = new self;
+    }
+
+    public function route()
+    {
+    }
+}
