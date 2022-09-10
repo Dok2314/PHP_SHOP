95dfbe9f7cb4356095312522fc6d5241dc1442ab
Dok2314 <koversundanil@gmail.com>
2022-09-10 16:24:07 +0300

    Small changes

--- core/base/controller/RouteController.php
+++ core/base/controller/RouteController.php
@@ -38,12 +38,14 @@ class RouteController
     {
         $address_str = $_SERVER['REQUEST_URI'];
 
+        // Если в запросе слеш последний / и это не корень сайта / то редирект на страницу без этого слеша для СЕО
         if(strrpos($address_str, '/') === strlen($address_str) - 1 && strrpos($address_str, '/') !== 0) {
             // $this->redirect(rtrim($address_str, '/'), 301);
         }
 
         $path = substr($_SERVER['PHP_SELF'], 0, strpos($_SERVER['PHP_SELF'],'index.php'));
 
+        // Если путь в конфиге равен тому за который отвечает скрипт то можем делать что-то если нет то у нас не настроен конфиг
         if($path === PATH) {
             $this->routes = Settings::getPropertyByName('routes');
 
@@ -76,18 +78,24 @@ class RouteController
     {
         $route = [];
 
+        //Если нулевой элемент не пуст то это не корень
+        // проверяем сузествует ли такой элиас,
+        // если да то из него берем первую чать - это котроллер
         if(!empty($url[0])) {
             if(isset($this->routes[$routeName]['routes'][$url[0]])) {
                 $route = explode('/', $this->routes[$routeName]['routes'][$url[0]]);
 
                 $this->controller .= ucfirst($route[0].'Controller');
             }else{
+                // если не существует то берем из строки запроса
                 $this->controller .= ucfirst($url[0].'Controller');
             }
         }else{
+            // если нет вообще строки запроса берём дефолтный
             $this->controller .= $this->routes['default']['controller'];
         }
 
+        // если нет методов берём из дефолта
         $this->inputMethod  = $route[1] ?? $this->routes['default']['inputMethod'];
         $this->outputMethod = $route[2] ?? $this->routes['default']['outputMethod'];
     }
