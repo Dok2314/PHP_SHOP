d2b3111e28e11134d15e5ef5d5d4d53dcb64038f
Dok2314 <koversundanil@gmail.com>
2022-09-17 17:16:28 +0300

    Added render method via render a templates

--- core/user/controller/IndexController.php
+++ core/user/controller/IndexController.php
@@ -6,5 +6,14 @@ use core\base\controller\BaseController;
 
 class IndexController extends BaseController
 {
+    protected function inputData()
+    {
+        $template = $this->render(false, ['name' => 'Daniil','age' => 19]);
+        exit($template);
+    }
 
+    protected function outputData()
+    {
+        
+    }
 }
