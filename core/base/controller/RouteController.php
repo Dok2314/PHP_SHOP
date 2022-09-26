ce0ea7b6cb3266ff4bdc7f02500e920e6c9a0224
Dok2314 <koversundanil@gmail.com>
2022-09-26 23:46:11 +0300

    Added system of logg

--- core/base/controller/RouteController.php
+++ core/base/controller/RouteController.php
@@ -24,7 +24,7 @@ class RouteController extends BaseController
         if($path === PATH) {
             $this->routes = Settings::getPropertyByName('routes');
 
-            if(!$this->routes) throw new RouteException('Сайт находится на техническом обслуживании!');
+            if(!$this->routes) throw new RouteException('Отсутствуют маршруты в базовых настройках!', 1);
 
             $url = explode('/', substr($address_str, strlen(PATH)));
 
@@ -97,11 +97,7 @@ class RouteController extends BaseController
                 }
             }
         }else{
-            try {
-                throw new \Exception('Не корректная директория сайта!');
-            }catch (\Exception $e) {
-                exit($e->getMessage());
-            }
+            throw new RouteException('Не корректная директория сайта!', 1);
         }
     }
 
